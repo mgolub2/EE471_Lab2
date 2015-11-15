41f1b05b5869b4f3c3940dd42fb161f3d6ee9c27
Max Golub <mgolub2@uw.edu>
2015-11-15 12:06:31 -0800

    Make 2x1 mux any width

--- Mux_32_2x1.sv
+++ Mux_32_2x1.sv
@@ -7,15 +7,18 @@ Author: Max Golub
 */
 
 module Mux_32_2x1(out, in, select);
+
+	parameter width = 32;
 	
-	output [31:0] out;
-	input [1:0][31:0] in;
+	output [width-1:0] out;
+	input [1:0][width-1:0] in;
 	input select;
+
 
 	genvar i;
 
 	generate
-		for (i=0; i < 32; i++) begin: muxgen
+		for (i=0; i < width; i++) begin: muxgen
 			Mux2x1 mux21(.out(out[i]), .in({in[1][i], in[0][i]}), .select(select));
 		end
 	endgenerate
